75be0dc8afe46a7d5e291a8cde6099656da8e085
happyqian_ah <happyqian123@gmail.com>
2017-11-05 23:27:52 +0800

    Finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,3 +5,7 @@
  * Date: 2017/11/5
  * Time: 21:45
  */
+
+function route_class(){
+    return str_replace('.','-',Route::currentRouteName());
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+    <div class="{{ route_class() }}-page" id="app">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+
+        @include('layouts._footer')
+    </div>
+
+    <!-- Script -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</html>
