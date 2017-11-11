1714d10835f3cf67835c412a868dc114a77de5f9
happyqian_ah <happyqian123@gmail.com>
2017-11-11 14:14:04 +0800

    User can create topic

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,3 +1,7 @@
 <div class="panel panel-default">
-    <div class="panel-body"> 右边导航栏 </div>
+    <div class="panel-body">
+        <a href="{{ route('topics.create') }}" aria-label="Left Align" class="btn btn-success btn-block">
+            <span class="glyphicon glyphicon-pencil"></span> 新建帖子
+        </a>
+    </div>
 </div>
