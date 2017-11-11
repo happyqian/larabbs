be1625497a5320f07ed82cb192da70d6d114a7ca
happyqian_ah <happyqian123@gmail.com>
2017-11-11 22:47:46 +0800

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -14,5 +14,10 @@ class ReplyObserver
         $reply->topic->increment('reply_count',1);
     }
 
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count',1);
+    }
+
 
 }
