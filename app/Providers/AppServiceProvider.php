1fe58b2f51eca7a8663012e583c1d2d8dfeeec9b
happyqian_ah <happyqian123@gmail.com>
2017-11-11 22:20:50 +0800

    User can create reply

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
         \Carbon\Carbon::setLocale('zh');
     }
 
